713e93fa7eebb5f7627c691e89ff7c0cf1411c4b
Joelp8 <pjoel6730@gmail.com>
2025-07-08 16:13:39 -0500

    Create Joelp8.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Joelp8.py
@@ -0,0 +1,24 @@
+# https://python.org/
+
+# comenbtario en euna linea
+
+"""
+comentario en varias lineas
+"""
+'''
+esto tambien es comentario en varias lineas
+'''
+my_variable = "mi variable"
+my_variable = "buevo valor de mi variable"
+
+MY_CONSTANT = "MI COSTANTE" #por convencion
+
+
+my_int = 1 #numero entero
+my_float = 1.2 #decimales
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de tecto"
+my_other_string = 'Mi otra cadena de texto'
+
+print ("¡Hola, Phyton!")
